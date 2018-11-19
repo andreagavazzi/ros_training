2fd4365be2ff5dfc1dceb4a2a091b9aac939c581
Andrea Gavazzi <andrea.gavazzi@me.com>
2018-11-19 14:57:35 +0100

    Update nodes.py

--- beginner/nodes.py
+++ beginner/nodes.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python
-import rospy
+#!/usr/bin/env python             # Sempre necessario
+import rospy                      # Importa le librerie ROS
 
 if __name__ == '__main__':
-  rospy.init_node('node_name')
-  rospy.loginfo('Node is up')
+  rospy.init_node('node_name')    # Inizializza il nodo
+  rospy.loginfo('Node is up')     # Comunica al log che il nodo è partito
+  
   rate = rospy.rate(10)
   
   while not rospy.is_shutdown():
