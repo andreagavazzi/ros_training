438fc35a79d5a3d3f3351641a2f78d5cc07f221e
Andrea Gavazzi <andrea.gavazzi@me.com>
2018-11-19 14:47:37 +0100

    Create nodes.py

---
+++ beginner/nodes.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import rospy
+
+if __name__ == '__main__':
+  rospy.init_node('node_name')
+  rospy.loginfo('Node is up')
+  rate = rospy.rate(10)
+  
+  while not rospy.is_shutdown():
+    # main code ....
